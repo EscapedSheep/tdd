f4764ec39a0654aee1393466470b60297ad3a384
EscapedSheep <sfgmdf001@126.com>
2020-08-03 11:36:21 +0800

    [maida Ge] feat: return stirng add mommy when given string have more than 30 percent of vowels

--- src/main/java/Mommifier.java
+++ src/main/java/Mommifier.java
@@ -1,6 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mommifier {
 
+    public static final String MOMMY = "mommy";
+
     String convert(String aString) {
-        return aString;
+        int length = aString.length();
+        int vowelsCount = countVowelsOfStr(aString);
+
+        if (vowelsCount/0.3 < length)
+            return aString;
+
+        char[]  charOfStr = aString.toCharArray();
+        List<String> convertResult = new ArrayList<>();
+        char previous = charOfStr[0];
+        convertResult.add(String.valueOf(previous));
+        for (int i = 1; i < charOfStr.length; i++) {
+            char current = charOfStr[i];
+            if (isVowel(previous) && !isVowel(current)) {
+                convertResult.add(MOMMY);
+            }
+            convertResult.add(String.valueOf(current));
+            previous = current;
+        }
+        if (isVowel(previous))
+            convertResult.add(MOMMY);
+
+        return String.join("",convertResult);
+
+    }
+
+    private Boolean isVowel(char aChar) {
+        switch (aChar) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                return true;
+        }
+        return false;
+    }
+
+    private int countVowelsOfStr(String aString) {
+        int vowelsCount = 0;
+        char[]  charOfStr = aString.toCharArray();
+        for (char c : charOfStr) {
+            if (isVowel(c))
+                vowelsCount += 1;
+        }
+        return vowelsCount;
     }
 }
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -11,4 +11,12 @@ public class MommifierTest {
         assertEquals(expected, strToConvert);
 
     }
+
+    @Test
+    void should_return_string_add_mommy_when_convert_given_string_have_more_than_30percent_of_vowels() {
+        Mommifier mommifier = new Mommifier();
+        String strToConvert = "apple";
+        String expected = mommifier.convert(strToConvert);
+        assertEquals(expected, "amommypplemommy");
+    }
 }
